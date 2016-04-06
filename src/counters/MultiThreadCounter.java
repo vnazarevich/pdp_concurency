3d11ef8fca90c79796ec13726fdf6eca1ec36611
vnazarevich <knp.lviv.nv@gmail.com>
2016-04-06 18:33:26 +0300

    change MultiTHreadCounter

--- src/counters/MultiThreadCounter.java
+++ src/counters/MultiThreadCounter.java
@@ -2,23 +2,31 @@ package counters;
 
 import java.io.File;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MultiThreadCounter extends Counter implements Callable{
+	private static ExecutorService executor = Executors.newFixedThreadPool(10);
+	private static ConcurrentHashMap<String, Integer> countResults = new ConcurrentHashMap<>();
 	private File rootFile;
 	
 	public MultiThreadCounter(File rootFile) {
 		this.rootFile = rootFile;
 	}
+	public MultiThreadCounter() {
+	}
 
 	@Override
 	protected void countFiles(File file) {
+		System.out.println("f = " + file.getName());
 		File[] files = file.listFiles();
 		if (files == null) {
 			return;
 		}
 		for (File f : files) {
 			if (f.isDirectory()){
-				countFiles(f);
+				executor.submit(new MultiThreadCounter(f));
 			} else{
 				addToCountResults(f.getName());
 			}
@@ -30,6 +38,17 @@ public class MultiThreadCounter extends Counter implements Callable{
 	public Object call() throws Exception {
 		countFiles(rootFile);
 		return null;
+	}
+	
+	@Override
+	public void addToCountResults(String fileName){
+		System.out.println("size = " + countResults.size());
+		if (countResults.containsKey(fileName)){
+			countResults.put(fileName, countResults.get(fileName) + 1);
+		}
+		else{
+			countResults.put(fileName, 1);
+		}
 	}
 
 }
--- src/pdp/nazarevych/concurency/Start.java
+++ src/pdp/nazarevych/concurency/Start.java
@@ -2,19 +2,25 @@ package pdp.nazarevych.concurency;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import counters.Counter;
+import counters.MultiThreadCounter;
 import counters.SimpleCounter;
 
 public class Start {
-	private static Set <Counter> counters = new HashSet<Counter>(){{
+	private static Set <Counter> counters  = new HashSet<Counter>(){{
 		add(new SimpleCounter());
+		add(new MultiThreadCounter());
 	}};
+	
+	//private static ExecutorService executor = Executors.newFixedThreadPool(10);
 
 	public static void main(String[] args) {
 		//startCounters("c:\\Test\\");
-		startCounters("C:\\");
-		//startCounters("G:\\");
+		//startCounters("C:\\");
+		startCounters("d:\\Private\\IT\\");
 	}
 	
 	private static void startCounters(String directoryPath){
@@ -23,7 +29,6 @@ public class Start {
 			counter.countFilesNumber(directoryPath);
 			counter.showStatistic();
 		}
-		
 	}
 
 }
